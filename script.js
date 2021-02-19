31e590a2c05b1f6a5ea1ce9d25010d64b67e9384
0xb0b1 <vcente82@gmail.com>
2021-02-19 19:30:07 -0300

    ...

---
+++ script.js
@@ -0,0 +1,13 @@
+const Modal = {
+    open() {
+        // abrir formulário
+        // Adicionar a classe `active`
+        alert("Abrir modal")
+    },
+
+    close() {
+        // fechar o formulário
+        // Remover a classe `active`
+        alert("Fechar modal")
+    }
+}
