c2c4f9d2b2176d74ddba66e60856a42225cb448d
0xb0b1 <vcente82@gmail.com>
2021-02-21 09:29:25 -0300

    ...

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ const transactions = [
     amount: -200,
     date: "22/01/2021",
   },
-];
+]
 
 const Transaction = {
     incomes() {
@@ -46,3 +46,19 @@ const Transaction = {
         // entradas - saídas
     }
 }
+
+// pegar as transações e colocar no HTML
+ const DOM = {
+     innerHTMLTransaction() {
+         const html = `
+         <tr>
+            <td class="description">Luz</td>
+            <td class="expense">- R$ 500,00</td>
+            <td class="date">23/01/2021</td>
+            <td>
+                <img src="./assets/minus.svg" alt="Remover transação">
+            </td>
+        </tr>
+         `
+     }
+ }
